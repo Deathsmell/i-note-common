0689c171d88562fe55902c2bfc60ce98f19fe757
Alexander Mitskevich <johndeathsmell@gmail.com>
2020-10-22 19:00:23 +0300

    deploy on heroku

---
+++ build/precache-manifest.2191266386e7fc7d3b82c5ad838a366b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f137689a8c9bb5dfc7fddfa4d7f732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd28800a14561f48389",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "1754907dad8baed29812",
+    "url": "/static/css/main.c9e7ca44.chunk.css"
+  },
+  {
+    "revision": "5fd28800a14561f48389",
+    "url": "/static/js/2.c6f1dfda.chunk.js"
+  },
+  {
+    "revision": "de25ba58efe4671c83476e5f3a907c26",
+    "url": "/static/js/2.c6f1dfda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1754907dad8baed29812",
+    "url": "/static/js/main.36abed98.chunk.js"
+  },
+  {
+    "revision": "24cd42e447cd6b2eacba",
+    "url": "/static/js/runtime-main.fab8ed63.js"
+  }
+]);
